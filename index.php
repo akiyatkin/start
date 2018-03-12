6e250b5c42db0f024cb653b42d9c32203b4ed492
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-03-12 17:41:12 +0400

    fix

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 namespace akiyatkin\start;
 use infrajs\rest\Rest;
 use infrajs\access\Access;
-use akiyatkin\boo\Once;
+use infrajs\once\Once;
 
 Access::debug(true);
 
@@ -12,7 +12,7 @@ Rest::get( function () {
 	$html = Rest::parse('-start/layout.tpl', $data);
 	echo $html;
 },'start', function(){
-	Once::setStartTime(); //Тестировщик будет обновлять кэш на сайте
+	Cache::setStartTime(); //Тестировщик будет обновлять кэш на сайте
 	echo 'Установлено новое начало времён<br>';
-	echo Once::getStartTime();
+	echo Cache::getStartTime();
 });
